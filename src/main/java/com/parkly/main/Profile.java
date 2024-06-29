c34276a071baabb0b1aacb5dd4e2723f702d0866
TingleVB <vanbalkomtom@gmail.com>
2024-06-30 09:34:08 +1000

    shit

--- src/main/java/com/parkly/main/Profile.java
+++ src/main/java/com/parkly/main/Profile.java
@@ -10,10 +10,10 @@ public class Profile extends Displayable {
         super(new DisplayObject((Graphics2D g2, Display display) -> {
             double hunger = animal.getHunger();
             String name = animal.getName();
-            g2.drawImage(Utils.toImage("image.png"), (int)(Window.WIDTH * 0.05), (int)(Window.HEIGHT * 0.3), (int)(Window.WIDTH * 0.9), (int)(Window.WIDTH * 0.9), display);
-            g2.drawImage(Utils.toImage(animal.getPhoto()), (int)(Window.WIDTH * 0.1), (int)(Window.HEIGHT * 0.6), (int)(Window.WIDTH * 0.4), (int)(Window.WIDTH * 0.4), display);
+            g2.drawImage(Utils.toImage("square.png"), (int)(Window.WIDTH * 0.05), (int)(Window.HEIGHT * 0.3), (int)(Window.WIDTH * 0.9), (int)(Window.WIDTH * 0.9), display);
+            g2.drawImage(Utils.toImage(animal.getPhoto()), (int)(Window.WIDTH * 0.1), (int)(Window.HEIGHT * 0.35), (int)(Window.WIDTH * 0.4), (int)(Window.WIDTH * 0.4), display);
 
-            g2.setFont(new Font("TimesRoman", Font.PLAIN, (int) (Window.HEIGHT * 0.1)));
+            g2.setFont(new Font("TimesRoman", Font.PLAIN, (int) (Window.HEIGHT * 0.07)));
             g2.drawString(name, (int)(Window.WIDTH * 0.15), (int)(Window.HEIGHT * 0.65));
             g2.drawString(animal.getSpecies().getName(), (int)(Window.WIDTH * 0.15), (int)(Window.HEIGHT * 0.7));
             g2.drawString("Health: " + (int)(100-hunger) + "/100", (int)(Window.WIDTH * 0.15), (int)(Window.HEIGHT * 0.75));
