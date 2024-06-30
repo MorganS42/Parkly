19047f0218e0c8b7e7585717accbcfaba4e5a3f0
MikidaSquid <58275032+MikidaSquid@users.noreply.github.com>
2024-06-30 11:31:55 +1000

    man.

--- src/main/java/com/parkly/main/PhotoLibrary.java
+++ src/main/java/com/parkly/main/PhotoLibrary.java
@@ -12,8 +12,8 @@ public class PhotoLibrary extends Displayable {
     public static final int PHOTOS_PER_PAGE = PHOTOS_PER_COL * PHOTOS_PER_ROW;
     public static final int FIRST_PHOTO_X = (int) Math.floor(Window.WIDTH * 0.05);
     public static final int FIRST_PHOTO_Y = (int) Math.floor(Window.HEIGHT * 0.05);
-    public static final int PHOTO_WIDTH = (int) Math.floor(Window.HEIGHT * 0.4);
-    public static final int PHOTO_HEIGHT = (int) Math.floor(Window.HEIGHT * 0.4);
+    public static final int PHOTO_WIDTH = (int) Math.floor(Window.HEIGHT * 0.2);
+    public static final int PHOTO_HEIGHT = (int) Math.floor(Window.HEIGHT * 0.2);
     public static final int PHOTO_GAP_X = (int) Math.floor(Window.HEIGHT * 0.1);
     public static final int PHOTO_GAP_Y = (int) Math.floor(Window.HEIGHT * 0.1);
 
